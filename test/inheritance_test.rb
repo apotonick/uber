f57139c8b36ad8b9673682e440a25da700bb166b
Nick Sutterer <apotonick@gmail.com>
2014-03-01 15:24:51 +1100

    this works, but has globals (InheritIncludedTo module).

--- test/inheritance_test.rb
+++ test/inheritance_test.rb
@@ -1,31 +1,38 @@
 require 'test_helper'
 require 'uber/inheritable_included'
 
+module InheritIncludedTo
+  def self.call(base)
+    base.class_eval do
+      puts "addin included to #{base}"
+      def self.included(b) #
+        CODE_BLOCK.call(b)
+
+        InheritIncludedTo.call(b)
+      end
+    end
+  end
+end
+
 class InheritanceTest < MiniTest::Spec
   module Feature
     #extend Uber::InheritedIncluded
 
-    def self.included(base)
-      super # from uber?
-      puts "selber"
-      base.class_eval { extend ClassMethods }
+    ::CODE_BLOCK = lambda { |base| base.class_eval { extend ClassMethods } } # i want that to be executed at every include
+
+
+    def self.included(base) #
+      CODE_BLOCK.call(base)
+      InheritIncludedTo.call(base)
     end
 
     module ClassMethods
       def feature; end
     end
-
   end
 
   module Extension
     include Feature
-
-    def self.included(base)
-puts base
-      #base.class_eval do
-        Feature.included(base)
-      #end
-    end
 
     # TODO: test overriding ::included
   end
