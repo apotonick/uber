b655a84ac63ae74b5d7ff17b3f30bcc667b3928d
Nick Sutterer <apotonick@gmail.com>
2014-03-01 13:58:08 +1100

    add a first test for inherited included.

--- test/inheritance_test.rb
+++ test/inheritance_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class InheritanceTest < MiniTest::Spec
-  describe "including" do
-    
+  module Feature
+    def self.included(base)
+      base.class_eval { def feature; end }
+    end
   end
+
+  module Extension
+    include Feature
+
+    # TODO: test overriding ::included
+  end
+
+  module Client
+    include Extension
+  end
+
+  it { Client.must_respond_to :feature }
 end
