e813061ccbe1bc06fd8e573ea6235a47bdafda7f
Nick Sutterer <apotonick@gmail.com>
2014-03-01 14:31:51 +1100

    make inheritable_included work manually. no use, yet.

--- test/inheritance_test.rb
+++ test/inheritance_test.rb
@@ -1,14 +1,31 @@
 require 'test_helper'
+require 'uber/inheritable_included'
 
 class InheritanceTest < MiniTest::Spec
   module Feature
+    #extend Uber::InheritedIncluded
+
     def self.included(base)
-      base.class_eval { def feature; end }
+      super # from uber?
+      puts "selber"
+      base.class_eval { extend ClassMethods }
     end
+
+    module ClassMethods
+      def feature; end
+    end
+
   end
 
   module Extension
     include Feature
+
+    def self.included(base)
+puts base
+      #base.class_eval do
+        Feature.included(base)
+      #end
+    end
 
     # TODO: test overriding ::included
   end
@@ -17,5 +34,6 @@ class InheritanceTest < MiniTest::Spec
     include Extension
   end
 
+  it { Extension.must_respond_to :feature }
   it { Client.must_respond_to :feature }
 end
