af33228bccf6043cb3b101ba7f9ef36d3f2cad16
Timo Schilling <timo@schilling.io>
2015-11-08 10:00:39 +0100

    change instance_eval style to allow code inspections

--- lib/uber/inheritable_attr.rb
+++ lib/uber/inheritable_attr.rb
@@ -2,7 +2,7 @@ module Uber
   module InheritableAttr
 
     def inheritable_attr(name, options={})
-      instance_eval %Q{
+      instance_eval <<-RUBY, __FILE__, __LINE__ + 1
         def #{name}=(v)
           @#{name} = v
         end
@@ -11,7 +11,7 @@ module Uber
           return @#{name} if instance_variable_defined?(:@#{name})
           @#{name} = InheritableAttribute.inherit_for(self, :#{name}, #{options})
         end
-      }
+      RUBY
     end
 
     def self.inherit_for(klass, name, options={})
