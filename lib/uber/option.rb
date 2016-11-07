ff953b0dc4d7055d0ad62692b4bbd1e60268d559
Nick Sutterer <apotonick@gmail.com>
2016-11-08 09:36:24 +1100

    introduce Uber::Option which supercedes Uber::Options::Value by simplifying it and speeding it up big time.

---
+++ lib/uber/option.rb
@@ -0,0 +1,24 @@
+require "uber/callable"
+
+module Uber
+  class Option
+    def self.[](value, options={}) # TODO: instance_exec: true
+      if value.is_a?(Proc)
+        return ->(context, *args) { context.instance_exec(*args, &value) } if options[:instance_exec]
+        return value
+      end
+
+      return value if value.is_a?(Uber::Callable)
+      return ->(context, *args){ context.send(value, *args) } if value.is_a?(Symbol)
+      new(value)
+    end
+
+    def initialize(value)
+      @value = value
+    end
+
+    def call(*)
+      @value
+    end
+  end
+end
